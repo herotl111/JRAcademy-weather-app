fe138cddfa9378697affdceeb3a5f1e09c0e3f4b
silverwind <charlieqiaoperth@gmail.com>
2019-05-05 00:24:54 +0800

    not really switch items

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -5,10 +5,10 @@ const Forecast = (props) => {
     return (
         <section class="weather-forecast">
             <div className="forecast__switch">
-                <button className="forecast__switch_0 switch-active">5 items</button>
-                <button className="forecast__switch_1">10 items</button>
+                <button onClick={props.swtichLabel} className="forecast__switch_0 switch-active">5 items</button>
+                <button onClick={props.swtichLabel} className="forecast__switch_1">10 items</button>
             </div>
-            <div className="weather-forecast__row">
+            <div className="weather-forecast__row ">
                 <span className="weather-forecast__day">{data[0].weekday}</span>
                 <span className="weather-forecast__icon">
                     <i className="weather-forecast__icon">{data[0].time}</i>
@@ -17,36 +17,60 @@ const Forecast = (props) => {
                 <span className="weather-forecast__low">{data[0].low[unit]} {unit}</span>
             </div>
             <div className="weather-forecast__row">
-                <span className="weather-forecast__day">{data[0].weekday}</span>
+                <span className="weather-forecast__day">{data[1].weekday}</span>
                 <span className="weather-forecast__icon">
-                    <i className="weather-forecast__icon">{data[0].time}</i>
+                    <i className="weather-forecast__icon">{data[1].time}</i>
                 </span>
-                <span className="weather-forecast__high">{data[0].high[unit]} {unit}</span>
-                <span className="weather-forecast__low">{data[0].low[unit]} {unit}</span>
+                <span className="weather-forecast__high">{data[1].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[1].low[unit]} {unit}</span>
             </div>
             <div className="weather-forecast__row">
-                <span className="weather-forecast__day">{data[0].weekday}</span>
+                <span className="weather-forecast__day">{data[2].weekday}</span>
                 <span className="weather-forecast__icon">
-                    <i className="weather-forecast__icon">{data[0].time}</i>
+                    <i className="weather-forecast__icon">{data[2].time}</i>
                 </span>
-                <span className="weather-forecast__high">{data[0].high[unit]} {unit}</span>
-                <span className="weather-forecast__low">{data[0].low[unit]} {unit}</span>
+                <span className="weather-forecast__high">{data[2].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[2].low[unit]} {unit}</span>
             </div>
             <div className="weather-forecast__row">
-                <span className="weather-forecast__day">{data[0].weekday}</span>
+                <span className="weather-forecast__day">{data[3].weekday}</span>
                 <span className="weather-forecast__icon">
-                    <i className="weather-forecast__icon">{data[0].time}</i>
+                    <i className="weather-forecast__icon">{data[3].time}</i>
                 </span>
-                <span className="weather-forecast__high">{data[0].high[unit]} {unit}</span>
-                <span className="weather-forecast__low">{data[0].low[unit]} {unit}</span>
+                <span className="weather-forecast__high">{data[3].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[3].low[unit]} {unit}</span>
             </div>
             <div className="weather-forecast__row">
-                <span className="weather-forecast__day">{data[0].weekday}</span>
+                <span className="weather-forecast__day">{data[4].weekday}</span>
                 <span className="weather-forecast__icon">
-                    <i className="weather-forecast__icon">{data[0].time}</i>
+                    <i className="weather-forecast__icon">{data[4].time}</i>
                 </span>
-                <span className="weather-forecast__high">{data[0].high[unit]} {unit}</span>
-                <span className="weather-forecast__low">{data[0].low[unit]} {unit}</span>
+                <span className="weather-forecast__high">{data[4].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[4].low[unit]} {unit}</span>
+            </div>
+            <div className={props.offLabel}>
+                <span className="weather-forecast__day">{data[5].weekday}</span>
+                <span className="weather-forecast__icon">
+                    <i className="weather-forecast__icon">{data[5].time}</i>
+                </span>
+                <span className="weather-forecast__high">{data[5].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[5].low[unit]} {unit}</span>
+            </div>
+            <div className={props.offLabel}>
+                <span className="weather-forecast__day">{data[6].weekday}</span>
+                <span className="weather-forecast__icon">
+                    <i className="weather-forecast__icon">{data[6].time}</i>
+                </span>
+                <span className="weather-forecast__high">{data[6].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[6].low[unit]} {unit}</span>
+            </div>
+            <div className={props.offLabel}>
+                <span className="weather-forecast__day">{data[7].weekday}</span>
+                <span className="weather-forecast__icon">
+                    <i className="weather-forecast__icon">{data[7].time}</i>
+                </span>
+                <span className="weather-forecast__high">{data[7].high[unit]} {unit}</span>
+                <span className="weather-forecast__low">{data[7].low[unit]} {unit}</span>
             </div>
         </section>
     );
--- src/components/WeatherChannel.jsx
+++ src/components/WeatherChannel.jsx
@@ -23,22 +23,46 @@ const mockConditionData = {
     {weekday:'Tue', time:'14:00', high:{C:29, F:70}, low:{C:18, F:58}},
     {weekday:'Tue', time:'15:00', high:{C:29, F:70}, low:{C:18, F:58}},
     {weekday:'Tue', time:'16:00', high:{C:29, F:70}, low:{C:18, F:58}},
+    {weekday:'Tue', time:'17:00', high:{C:29, F:70}, low:{C:18, F:58}},
+    {weekday:'Tue', time:'18:00', high:{C:29, F:70}, low:{C:18, F:58}},
+    {weekday:'Tue', time:'19:00', high:{C:29, F:70}, low:{C:18, F:58}},
   ];
   
 
 export default class WeatherChannel extends Component{
     constructor(props) {
         super(props);
-        this.state={unit:"C"}
+        this.state={
+            unit:"C",
+            switchButtonFew:"forecast__switch_0 switch-active",
+            switchButtonMore:"forecast__switch_0",
+            offLabel:"weather-forecast__row onoffswitch-checkbox"
+        };
     }
     
     swapTempUnit = () => {
-        this.setState ((preState, props) => {
-            if (preState.unit==="C") {
-                return ({unit:"F"});
+        this.setState ((preUnit, props) => {
+            if (preUnit.unit==="C") {
+                return ({unit:"F",});
             }
             return ({unit:"C"});            
             });
+    }
+    swtichLabel =() => {
+        
+        this.setState ((preState, props) =>{
+            if (preState.offLabel==="weather-forecast__row onoffswitch-checkbox" 
+            && preState.switchButtonFew==="forecast__switch_0 switch-active")   
+            {return ({offLabel:"weather-forecast__row",
+                    switchButtonFew:"forecast__switch_0",
+                    switchButtonMore:"forecast__switch_0 switch-active"
+                    });}
+            
+            return ({offLabel:"weather-forecast__row onoffswitch-checkbox",
+            switchButtonFew:"forecast__switch_0 switch-active",
+            switchButtonMore:"forecast__switch_0"
+            })
+        });
     }
     render(){
         return(
@@ -54,6 +78,8 @@ export default class WeatherChannel extends Component{
                 <Forecast 
                     data={mockForecastData} 
                     unit={this.state.unit}
+                    offLabel={this.state.offLabel}
+                    swtichLabel={this.swtichLabel}
                  />                 
             </main>
         );
